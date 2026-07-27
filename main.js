397f2cb616a2e95539e6b968a118ebc88d94c92b
konstantinos32 <konstantinostheodoropoulosphoto@hotmail.com>
2026-07-27 16:14:09 +0300

    Added some Pokemon API fetching logic

--- main.js
+++ main.js
@@ -9,6 +9,23 @@ const searchBtn = document.querySelector("#searchBtn");
 const apiUrl = "https://pokeapi.co/api/v2/pokemon";
 
 // Functions
+async function loadPokemon() {
+    const response= await fetch(apiUrl);
+    const data= await response.json();
+  
+     for (const pokemon of data.results) {
+
+        getPokemonDetails(pokemon.url);
+
+}
+
+}
+
+async function getPokemonDetails(url) {
+    const response= await fetch(url);
+    const pokemon= await response.json();
+    console.log(pokemon);
+}
 
 
 
@@ -17,3 +34,4 @@ const apiUrl = "https://pokeapi.co/api/v2/pokemon";
 
 
 // Start App
+loadPokemon();
