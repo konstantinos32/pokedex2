657919d730dbfb6beda83e623c374e3bc9e04ffd
konstantinos32 <konstantinostheodoropoulosphoto@hotmail.com>
2026-07-27 11:05:33 +0300

    Add project boilerplate

---
+++ main.js
@@ -0,0 +1,19 @@
+console.log("Main page loaded");
+
+// DOM Elements
+const pokemonContainer = document.querySelector("#pokemonContainer");
+const searchInput = document.querySelector("#searchInput");
+const searchBtn = document.querySelector("#searchBtn");
+
+// Variables
+const apiUrl = "https://pokeapi.co/api/v2/pokemon";
+
+// Functions
+
+
+
+// Event Listeners
+
+
+
+// Start App
