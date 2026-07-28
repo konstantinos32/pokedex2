14176d140514e226097fa323715518f4cf9d10ba
konstantinos32 <konstantinostheodoropoulosphoto@hotmail.com>
2026-07-28 11:44:48 +0300

    Created the display Pokemon cards from PokeAPI

--- main.js
+++ main.js
@@ -24,7 +24,31 @@ async function loadPokemon() {
 async function getPokemonDetails(url) {
     const response= await fetch(url);
     const pokemon= await response.json();
-    console.log(pokemon);
+    displayPokemon(pokemon);
+}
+function displayPokemon(pokemon) {
+
+    //Card creation
+    const card= document.createElement("div");
+    card.className="bg-white round-lg shadow-md p-4";
+
+    //Image creation
+    const image=document.createElement("img");
+    image.src= pokemon.sprites.front_default;
+    image.alt=pokemon.name;
+    image.className="mx-auto";
+
+    //name creation
+    const name= document.createElement("h2");
+    name.textContent=pokemon.name;
+    name.className = "text-xl font-bold text-center mt-3 capitalize";
+
+    //image and name addition to the card
+    card.appendChild(image);
+    card.appendChild(name);
+
+    // card added to the page
+    pokemonContainer.appendChild(card);
 }
 
 
