967e70ae2aa1d2b94fc9419f880b49a381f2fae3
Annemarie Neumann <annemarieneumann@Annemaries-MacBook-Air.local>
2026-07-28 08:48:19 +0200

    Added pokedex funtionality no styling yet

--- pokedex.js
+++ pokedex.js
@@ -1,19 +1,78 @@
-console.log("Main page loaded");
+console.log("Pokédex page loaded");
 
 // DOM Elements
-const pokemonContainer = document.querySelector("#pokemonContainer");
-const searchInput = document.querySelector("#searchInput");
-const searchBtn = document.querySelector("#searchBtn");
+const savedPokemonContainer = document.querySelector(
+    "#savedPokemonContainer"
+);
 
 // Variables
-const apiUrl = "https://pokeapi.co/api/v2/pokemon";
+const caughtPokemon =
+    JSON.parse(localStorage.getItem("caughtPokemon")) || [];
 
 // Functions
 
+function displayPokemon() {
+    if (caughtPokemon.length === 0) {
+        savedPokemonContainer.textContent =
+            "You have not caught any Pokémon yet.";
+        return;
+    }
 
+    caughtPokemon.forEach((pokemon) => {
+        const card = document.createElement("article");
 
-// Event Listeners
+        const image = document.createElement("img");
+        image.src = pokemon.image;
+        image.alt = pokemon.name;
 
+        const name = document.createElement("h2");
+        name.textContent = pokemon.name;
 
+        const hp = document.createElement("p");
+        hp.textContent = `HP: ${pokemon.hp}`;
+
+        const attack = document.createElement("p");
+        attack.textContent = `Attack: ${pokemon.attack}`;
+
+        const defense = document.createElement("p");
+        defense.textContent = `Defense: ${pokemon.defense}`;
+
+        const speed = document.createElement("p");
+        speed.textContent = `Speed: ${pokemon.speed}`;
+
+        const releaseButton = document.createElement("button");
+        releaseButton.textContent = "Release";
+
+        releaseButton.addEventListener("click", () => {
+            releasePokemon(pokemon.id);
+        });
+
+        function releasePokemon(pokemonId) {
+    const updatedPokemon = caughtPokemon.filter(
+        (pokemon) => pokemon.id !== pokemonId
+    );
+
+    localStorage.setItem(
+        "caughtPokemon",
+        JSON.stringify(updatedPokemon)
+    );
+
+    location.reload();
+}
+
+        card.append(
+            image,
+            name,
+            hp,
+            attack,
+            defense,
+            speed,
+            releaseButton
+        );
+
+        savedPokemonContainer.appendChild(card);
+    });
+}
 
 // Start App
+displayPokemon();
